402bae28baabde75d071016c36ee44f5677d6cdd
devilgates <y2-matsumoot@nri.co.jp>
2017-03-11 14:06:57 +0900

    Add files via upload

---
+++ biography.js
@@ -0,0 +1,5 @@
+function openBIO(url, wid)
+{
+    var newWin;
+    newWin = window.open(url, wid, "menubar=no,toolbar=no,location=no,directories=no,status=yes,scrollbars=yes,resizable=yes,width=586,height=450,left=110,top=20");
+}
